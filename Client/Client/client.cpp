3e6eb4f2a1e4dbb5a3cbca3c5e0a1004fefe3c6d
eramaliqi <122036080+eramaliqi@users.noreply.github.com>
2023-12-01 18:00:07 +0100

    Update on client.cpp

--- Client/Client/client.cpp
+++ Client/Client/client.cpp
@@ -27,5 +27,36 @@ void ClientThread()
 
 int main()
 {
+    WSAData wsaData;
+    WORD DllVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DllVersion, &wsaData) != 0)
+    {
+        MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+    
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr(""); // IP Adresa
+    addr.sin_port = htons(); // Porti
+    addr.sin_family = AF_INET;
+    
+    Connection = socket(AF_INET, SOCK_STREAM, NULL);
+    
+    if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0)
+    {
+        MessageBoxA(NULL, "Failed to Connect", "Error", MB_OK | MB_ICONERROR);
+        return 0; // Failed to Connect
+    }
+
+    std::cout << "Connected to server: SERVER_IP_ADDRESS" << std::endl;
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL);
+    char buffer[256] = "test";
+    while (true)
+    {
+        std::cin.getline(buffer, sizeof(buffer));
+        send(Connection, buffer, sizeof(buffer), NULL);
+        Sleep(10);
+    }
     return 0;
 }
