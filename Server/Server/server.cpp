3160847c257aa0be60822e3e568f68e645294adf
Dëshira Randobrava <121398589+d3shira@users.noreply.github.com>
2023-12-02 20:12:00 +0100

    Update server.cpp

--- Server/Server/server.cpp
+++ Server/Server/server.cpp
@@ -1,82 +1,187 @@
-#pragma warning(disable: 4996)
-
 #include <WinSock2.h>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <thread>
 
 #pragma comment(lib, "ws2_32.lib")
 
-SOCKET Connections[100];
-int ConnectionCounter = 0;
+#pragma warning(disable : 4996)
 
-void ClientHandlerThread(int index)
+#define BUFLEN 512
+#define PORT 8889
+
+void writeFile(const string& filename, const string& content)
 {
-    char buffer[256];
-    while (true)
+    ofstream file(filename, ios::out | ios::binary);
+    if (file.is_open())
     {
-        int bytesReceived = recv(Connections[index], buffer, sizeof(buffer), NULL);
-        if (bytesReceived <= 0)
-        {
-            std::cout << "Client disconnected" << std::endl;
-            closesocket(Connections[index]);
-            return;
-        }
-
-        std::string ClientName = "Client > ";
-        std::string Format = buffer;
-        std::string Final = ClientName + Format;
-
-        std::cout << Final.c_str() << std::endl;
-
-        for (int i = 0; i < ConnectionCounter; i++)
-        {
-            if (i == index)
-                continue;
-            send(Connections[i], buffer, sizeof(buffer), NULL);
-        }
+        file << content;
+        file.close();
+    }
+    else
+    {
+        cout << "Error opening file: " << filename << endl;
     }
 }
 
+string readFile(const string& filename)
+{
+    ifstream file(filename, ios::binary);
+    if (file.is_open())
+    {
+        string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+        file.close();
+        return content;
+    }
+    else
+    {
+        cout << "Error opening file: " << filename << endl;
+        return "";
+    }
+}
+
+void handleClient(SOCKET client_socket, sockaddr_in clientAddr, unordered_set<string>& allowedWriteClients)
+{
+    char message[BUFLEN] = {};
+
+    while (true)
+    {
+        int message_len = recv(client_socket, message, BUFLEN, 0);
+        if (message_len == SOCKET_ERROR)
+        {
+            cout << "recv() failed with error code: " << WSAGetLastError() << endl;
+            closesocket(client_socket);
+            break;
+        }
+        else if (message_len == 0)
+        {
+            cout << "Connection closed by " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) << endl;
+            closesocket(client_socket);
+            break;
+        }
+
+        string clientKey = string(inet_ntoa(clientAddr.sin_addr)) + ":" + to_string(ntohs(clientAddr.sin_port));
+
+        if (strncmp(message, "file r: ", 8) == 0)
+        {
+            string filename = message + 8;
+            
+            string fileContent = readFile(filename);
+
+            send(client_socket, fileContent.c_str(), fileContent.size(), 0);
+        }
+        else if (strncmp(message, "file w: ", 8) == 0)
+        {
+            if (allowedWriteClients.empty() || allowedWriteClients.count(clientKey) > 0)
+            {
+                string fileWriteCommand = message + 8;
+                size_t pos = fileWriteCommand.find(' ');
+                if (pos != string::npos)
+                {
+                    string filename = fileWriteCommand.substr(0, pos);
+                    string content = fileWriteCommand.substr(pos + 1);
+
+                    writeFile(filename, content);
+
+                    string successMsg = "Content written to file: " + filename;
+                    send(client_socket, successMsg.c_str(), successMsg.size(), 0);
+
+                    allowedWriteClients.insert(clientKey);
+                }
+            }
+            else
+            {
+                string errorMsg = "Write operation not allowed for this client";
+                send(client_socket, errorMsg.c_str(), errorMsg.size(), 0);
+            }
+        }
+        else
+        {
+            printf("Client says: %s\n", message);
+
+            char serverMessage[BUFLEN];
+            printf("Enter message to client: ");
+            cin.getline(serverMessage, BUFLEN);
+
+            send(client_socket, serverMessage, strlen(serverMessage), 0);
+        }
+
+
+        memset(message, 0, BUFLEN);
+    }
+}
 int main() {
    
-    WSAData wsaData;
-    WORD DllVersion = MAKEWORD(2, 2);
-    if (WSAStartup(DllVersion, &wsaData) != 0) 
+      system("title TCP Server");
+
+    WSADATA wsa;
+    printf("Initialising Winsock...");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
-        MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);
-        return 0;
+        printf("Failed. Error Code: %d", WSAGetLastError());
+        exit(0);
+    }
+    printf("Initialised.\n");
+
+    SOCKET server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        printf("Could not create socket: %d", WSAGetLastError());
+    }
+    printf("Socket created.\n");
+
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr("192.168.178.28");               //IP adresa 
+
+    if (server.sin_addr.s_addr == INADDR_NONE) {
+        printf("Invalid IP address: %s", "192.168.178.28");              //IP adresa
+        exit(EXIT_FAILURE);
+    }
+    server.sin_port = htons(PORT);
+
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code: %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+    puts("Bind done.");
+
+    if (listen(server_socket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        printf("Listen failed with error code: %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+    puts("Waiting for incoming connections...");
+
+    unordered_set<string> allowedWriteClients;
+
+    while (true)
+    {
+        SOCKET client_socket;
+        sockaddr_in clientAddr;
+        int clientAddrSize = sizeof(clientAddr);
+
+        client_socket = accept(server_socket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (client_socket == INVALID_SOCKET)
+        {
+            printf("Accept failed with error code: %d", WSAGetLastError());
+            closesocket(server_socket);
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Connection accepted from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+        thread clientThread(handleClient, client_socket, clientAddr, ref(allowedWriteClients));
+        clientThread.detach(); 
     }
 
-    SOCKADDR_IN addr;                 
-    int addrlen = sizeof(addr);      
-    addr.sin_addr.s_addr = INADDR_ANY; 
-    addr.sin_port = htons(1111);       
-    addr.sin_family = AF_INET;         
-
-    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); 
-    bind(sListen, (SOCKADDR*)&addr, sizeof(addr));      
-    listen(sListen, SOMAXCONN);                      
-
-    SOCKET newConnection; 
-    for (int i = 0; i < 100; i++)
-    {
-        newConnection = accept(sListen, (SOCKADDR*)&addr, &addrlen); 
-        if (newConnection == 0)                                    
-        {
-            std::cout << "Failed to accept the client's connection." << std::endl;
-        }
-        else 
-        {
-            std::cout << "Client Connected" << std::endl;
-            char MOTD[256] = "This is a message sent from the server"; 
-            send(newConnection, MOTD, sizeof(MOTD), NULL);           
-            Connections[i] = newConnection;                            
-            ConnectionCounter += 1;                                  
-            CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandlerThread, (LPVOID)(i), NULL, NULL); 
-        }
-    }
-
-    system("pause");
+    closesocket(server_socket);
+    WSACleanup();
     return 0;
 }
 
